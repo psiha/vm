9ec5d2dbb298e07734cf0336fbe80e227911499d
psiha <psiha@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-08-29 16:11:40 +0000

    Added a convenience .cpp file for non header-only builds.
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/mmap@74139 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/mmap/amalgamated_lib.cpp
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+///
+/// \file amalgamated_lib.cpp
+/// -------------------------
+///
+/// Copyright (c) Domagoj Saric 2011.
+///
+///  Use, modification and distribution is subject to the Boost Software License, Version 1.0.
+///  (See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt)
+///
+/// For more information, see http://www.boost.org
+///
+////////////////////////////////////////////////////////////////////////////////
+//------------------------------------------------------------------------------
+
+#ifdef BOOST_MMAP_HEADER_ONLY
+    #error this file is meant for non header only builds
+#endif // BOOST_MMAP_HEADER_ONLY
+
+#include "handles/handle.inl"
+//..zzz...by utility.inl...#include "mappble_objects/file/file.inl"
+#include "mappble_objects/file/flags.inl"
+#include "mappble_objects/file/utility.inl"
+//#include "mappble_objects/shared_memory/posix/flags.inl"
+//#include "mappble_objects/shared_memory/win32/flags.inl"
+#include "mapped_view.inl"
+#include "mapping/mapping.inl"
+//------------------------------------------------------------------------------
