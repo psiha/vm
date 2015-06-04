d8e52f3239ab5397e5eb2ab2e95004cf50d68e60
Domagoj Šarić <dsaritz@gmail.com>
2015-06-04 23:34:40 +0200

    Forgot to include in a previous commit.

--- include/boost/mmap/mappble_objects/file/win32/file.hpp
+++ include/boost/mmap/mappble_objects/file/win32/file.hpp
@@ -3,11 +3,12 @@
 /// \file file.hpp
 /// --------------
 ///
-/// Copyright (c) Domagoj Saric 2010.-2013.
+/// Copyright (c) Domagoj Saric 2010 - 2015.
 ///
-///  Use, modification and distribution is subject to the Boost Software License, Version 1.0.
-///  (See accompanying file LICENSE_1_0.txt or copy at
-///  http://www.boost.org/LICENSE_1_0.txt)
+/// Use, modification and distribution is subject to the
+/// Boost Software License, Version 1.0.
+/// (See accompanying file LICENSE_1_0.txt or copy at
+/// http://www.boost.org/LICENSE_1_0.txt)
 ///
 /// For more information, see http://www.boost.org
 ///
@@ -17,11 +18,9 @@
 #define file_hpp__FB482005_18D9_4E3B_9193_A13DBFE88F45
 #pragma once
 //------------------------------------------------------------------------------
-#include "../../../implementations.hpp"
-#include "../../../mapping/mapping.hpp"
-#include "../handle.hpp"
-
-#include "boost/cstdint.hpp"
+#include "boost/mmap/implementations.hpp"
+#include "boost/mmap/mapping/mapping.hpp"
+#include "boost/mmap/mappble_objects/file/handle.hpp"
 
 #include <cstddef>
 //------------------------------------------------------------------------------
@@ -36,20 +35,19 @@ template <typename Impl  > struct file_open_flags;
 template <typename Impl  > struct file_mapping_flags;
 template <class    Handle> struct is_resizable;
 
-template <> struct is_resizable< file_handle<win32> > : mpl::true_ {};
+template <> struct is_resizable<file_handle<win32>> : std::true_type {};
+
+file_handle<win32> BOOST_CC_REG create_file( char    const * file_name, file_open_flags<win32> ) noexcept;
+file_handle<win32> BOOST_CC_REG create_file( wchar_t const * file_name, file_open_flags<win32> ) noexcept;
+bool               BOOST_CC_REG delete_file( char    const * file_name, win32                  ) noexcept;
+bool               BOOST_CC_REG delete_file( wchar_t const * file_name, win32                  ) noexcept;
 
 
-file_handle<win32> create_file( char    const * file_name, file_open_flags<win32> const & );
-file_handle<win32> create_file( wchar_t const * file_name, file_open_flags<win32> const & );
-bool               delete_file( char    const * file_name, win32                          );
-bool               delete_file( wchar_t const * file_name, win32                          );
+bool        BOOST_CC_REG set_size( file_handle<win32>::reference, std::size_t desired_size ) noexcept;
+std::size_t BOOST_CC_REG get_size( file_handle<win32>::reference                           ) noexcept;
 
 
-bool        set_size( file_handle<win32>::reference, std::size_t desired_size );
-std::size_t get_size( file_handle<win32>::reference                           );
-
-
-mapping<win32> create_mapping( file_handle<win32>::reference, file_mapping_flags<win32> const & );
+mapping<win32> BOOST_CC_REG create_mapping( handle<win32>::reference, file_mapping_flags<win32> ) noexcept;
 
 //------------------------------------------------------------------------------
 } // namespace mmap
