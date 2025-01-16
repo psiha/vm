bb2aaf937c5b9a148111d6c8079d333bef44f5cf
Domagoj Saric <dsaritz@gmail.com>
2025-01-16 11:16:42 +0100

    Clang 19: fixed (PCH) compilation errors.

--- src/pch.hpp
+++ src/pch.hpp
@@ -1,20 +1,29 @@
 #pragma once
 
+
 #include <psi/build/disable_warnings.hpp>
+#if _LIBCPP_VERSION && ( _LIBCPP_VERSION != 190106 ) // weird compilation errors mentioning va_list
 #include <psi/err/fallible_result.hpp>
+#endif
 
 #include <boost/assert.hpp>
+#if _LIBCPP_VERSION && ( _LIBCPP_VERSION != 190106 )
 #include <boost/config_ex.hpp>
+
 #include <boost/stl_interfaces/iterator_interface.hpp>
 #include <boost/stl_interfaces/sequence_container_interface.hpp>
+#endif
 
 #include <algorithm>
 #include <array>
+#include <std_fix/bit>
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
+#if _LIBCPP_VERSION && ( _LIBCPP_VERSION != 190106 )
 #include <functional>
+#endif
 #include <iterator>
 #include <limits>
 #include <memory>
@@ -26,7 +35,6 @@
 #include <type_traits>
 #include <tuple>
 #include <utility>
-#include <std_fix/bit>
 
 #ifdef _WIN32
 #include <windows.h>
